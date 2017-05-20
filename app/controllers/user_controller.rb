3783be7363b42a9653b699c40097536e744a12fb
AJ <ajoeford@gmail.com>
2017-05-19 20:14:28 -0700

    Sign up and login pages now functioning

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,18 +5,49 @@ class UserController < ApplicationController
   end
 
   get '/signup' do
-    erb :"users/new"
+    if logged_in?
+      redirect '/'
+    else
+      erb :"users/new"
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/'
+    else
+      erb :"users/login"
+    end
+  end
+
+  get '/users/logout' do
+    if logged_in?
+      session.clear
+      redirect '/'
+    else
+      redirect '/'
+    end
   end
 
   post '/users' do
-    user = User.new(username: params[:username], email: params[:email],
-      password: params[:password])
+    user = User.new(params[:user])
     if user.save
       session[:user_id] = user.id
       redirect "/#{user.id}/"
     else
       flash[:message] = "Failed to create new user."
       redirect "/signup"
+    end
+  end
+
+  post '/users/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/#{user.id}/"
+    else
+      flash[:message] = "Login failed."
+      redirect "/login"
     end
   end
 
