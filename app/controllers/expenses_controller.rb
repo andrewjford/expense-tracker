08acceb28bb7079b90674da10c60fb1f6d930ce4
AJ <ajoeford@gmail.com>
2017-05-20 15:41:53 -0700

    started add expense functionality, need to do add category first

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,18 @@
 class ExpensesController < ApplicationController
 
   get '/expenses/new' do
+    @categories = Category.all
     erb :"expenses/new"
   end
+
+  post '/expenses' do
+    if new_category = params[:new_category] != ""
+      category = Category.find_or_create_by(name: new_category)
+    else
+      category = Category.find_by(name: params[existing_category])
+    end
+
+    redirect "/#{current_user.id}/"
+  end
+
 end
